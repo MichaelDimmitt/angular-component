9c314738f298f528f88321e4cfa219a710699c05
MichaelDimmitt <michaelgdimmitt@gmail.com>
2019-10-19 17:42:36 -0400

    Scaffolds included in details of commit
    ng generate module CustomerDashboard
    
    ng generate component customer-dashboard/CustomerDashboard

---
+++ src/app/customer-dashboard/customer-dashboard.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
+
+
+
+@NgModule({
+  declarations: [CustomerDashboardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CustomerDashboardModule { }
