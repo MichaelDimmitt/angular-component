6cd0e986a2ba0de20e250190c1224a4b141a4200
MichaelDimmitt <michaelgdimmitt@gmail.com>
2019-10-20 12:55:01 -0400

    show dashboard at 'root' path, show 404 to invalid paths.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Page404Component } from './page404/page404.component';
-
+import { AppComponent } from './app.component'
 const routes: Routes = [
   { path: 'customer-list',
     loadChildren: () => import('./customer-dashboard/customer-dashboard.module').then(m => m.CustomerDashboardModule) 
   },
+  { path: '',
+    loadChildren: () => import('./customer-dashboard/customer-dashboard.module').then(m => m.CustomerDashboardModule) 
+  },
   {
-    path: '',
+    path: '**',
     component: Page404Component,
   }
 ];
