f246b08c57b0713a9f44c1f9fe203f689c462cc7
MichaelDimmitt <michaelgdimmitt@gmail.com>
2020-04-05 00:11:37 -0400

    add content and use ngTemplate outlet to break up the code

--- src/app/customer-dashboard/customer-dashboard/customer-dashboard.component.ts
+++ src/app/customer-dashboard/customer-dashboard/customer-dashboard.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerDashboardComponent implements OnInit {
 
+  listDirections = [
+    'north',
+    'south',
+    'east',
+    'west'
+  ]
+
   constructor() { }
 
   ngOnInit() {
